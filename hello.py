197d5f9749e2b91d283c15db715714d34a4ae147
Rodolfo <rlopez1@appdev.yearup.org>
2024-10-21 15:57:52 -0500

    lab 1 and 2

--- hello.py
+++ hello.py
@@ -10,3 +10,5 @@ cents = 0.50
 print(type(dollars + cents))
 
 print('3 dollars' + ' ' + 'fifty cents')
+
+
---
+++ variables_examples.py
@@ -0,0 +1,19 @@
+# 
+customer_id = 1 # simple numeric ID
+first_name = 'Juan' # First name string
+middle_name = 'David' # Middle name string
+last_name = 'Alejandre' # Last name string
+Gender = 'Male' # String for name 
+date_of_birth = '1999-09-31' # YYYY-MM-DD
+driver_license_number = 'JDA1345' # Needed letter and numbers
+auto_policy_number = 'POL1234' # Similar to license number
+
+my_name = 'Rodolfo' # My name 
+city_of_birth = 'Chicago' # My city 
+state_of_birth = 'Illinois' # My state
+
+# is: used to check if two variables are equal 
+# as: creates an alias
+# not: used as a logical operator to flip values.
+# or: used as a logical operator to return true if one statement is true
+# try: statement starts a block of code
