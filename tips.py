f6ed6102c45f4e376613d547b1973ef724f48e16
Rodolfo <rlopez1@appdev.yearup.org>
2024-10-22 10:47:19 -0500

    made calculations in python

---
+++ tips.py
@@ -0,0 +1,28 @@
+# Define known values
+food_cost = 79.25
+tax = 6.54
+tip = 12.00
+
+# Calculate the unknown
+total_due = food_cost + tax + tip
+
+# Display the results
+# print("Food cost is " + str(food_cost) + " and tax is " + str(tax))
+# print("Tip is " + format(tip, ".2f"))
+# print("Total due is " + str(total_due))
+
+# Bonus Q: you cannot copy and paste because of formatting in pdf's won't capture special characters
+
+
+# Define known values
+food_cost = 79.25
+tax = 6.54
+tip = 12.00
+
+# Calculate the unknown
+total_due = food_cost + tax + tip
+
+# Display the results
+print("Food cost is " + str(food_cost) + " and tax is " + str(tax))
+print("Tip is " + format(tip, ".2f"))
+print("Total due is " + str(total_due))
