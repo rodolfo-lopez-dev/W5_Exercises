2f93393013b291576d43d22c689b4df2973041c4
Rodolfo <rlopez1@appdev.yearup.org>
2024-10-28 11:39:18 -0500

    did the loops love exercise

---
+++ LoopsScripts/loop_love.py
@@ -0,0 +1,13 @@
+# Author: Rodolfo
+# Date: October 28th
+# This script prints phrases using a list of positive words.
+
+# List of words that mean "great"
+words = ["amazing", "awesome", "excellent", "fantastic", "wonderful"]
+
+# Print a phrase for each word.
+for word in words:
+    print(f"Loops are {word}!")
+
+# Results
+print("I <3 loops")
